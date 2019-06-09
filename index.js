701a48c7c62a347ace61502c72b24242c1e3bd50
Hassan Abusalih <nichaes1721@gmail.com>
2019-06-09 02:22:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 function shout(string) {
   return string.toUpperCase()
 }
+
 function whisper(string) {
   return string.toLowerCase()
 }
+
 function logShout(string) {
   console.log(
     string.toUpperCase()
   )
 }
+
 function whisper(string) {
   console.log(
     string.toLowerCase()
   )
 }
+
 function sayHiToGrandma(string) {
   if (string.UpperCase === string){
   return "YES INDEED!"
